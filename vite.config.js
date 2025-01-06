e8f4f9bb7fe7f5863aaa4a84f72b158166c26f67
Huaruying <Ruying1009@gmail.com>
2025-01-06 21:01:50 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+export default {
+    root: '.', // 設置根目錄為專案的根目錄
+    build: {
+      outDir: 'dist', // 默認輸出目錄
+    },
+    server: {
+      port: 8080, // 指定伺服器端口
+    },
+  };
+  
